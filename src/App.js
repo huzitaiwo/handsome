0bde9b315827f95203faf19ec37b5915eada2637
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-15 14:48:22 +0100

    initializing repository

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+// styles 
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App
