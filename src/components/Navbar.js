e43b8fa6d967843eaae2a4009d0877aadd771f2d
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-15 11:07:13 +0100

    styling navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,15 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <nav className='navbar'>
-      <Link to='/' className="brand">
-        <span>Handsome</span>
-      </Link>
-      <ul>
-        <li><Link to='/project'>Project</Link></li>
-        <li><Link to='/contact'>Let's Talk</Link></li>
-      </ul>
+      <div className="container">
+        <Link to='/' className="brand">
+          <span>Handsome</span>
+        </Link>
+        <ul>
+            <li><Link to='/project'>Project</Link></li>
+            <li><Link to='/contact'>Let's Talk</Link></li>
+        </ul>
+      </div>
     </nav>
   )
 }
