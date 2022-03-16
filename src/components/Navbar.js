c11662aef2122ebe08afa6b48460dd6ce51734fe
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-16 13:03:27 +0100

    added brand icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
+import brand from '../assest/logo.svg'
 
+// style
 import './Navbar.css'
-
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
       <div className="container">
         <Link to='/' className="brand">
+          <img src={brand} alt="brand" />
           <span>Handsome</span>
         </Link>
         <ul>
