b726f4519a3f1bda66cba77a83a74bb5c17bbac4
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-16 12:46:32 +0100

    banner template

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,9 +4,9 @@ export default function Banner() {
   return (
     <section className='banner'>
       <div className="container">
-        <span>HI</span>
-        <h1>I'm Hussen Tawio</h1>
-        <h3>Frontend Developer and Software Engineer from somewhere</h3>  
+        <span>&#128075; Hi there</span>
+        <h2>I'm a Frontend Developer and Software Engineer from somewhere</h2>
+        <p>I have 3 years of experience of building and developing websites and web applications for various organizations</p> 
       </div>
     </section>
   )
