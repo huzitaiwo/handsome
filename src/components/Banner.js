f2f917830eea0f13f017156e76ba6fbb859ce9d7
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-16 13:00:04 +0100

    added assest folder

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,13 @@ export default function Banner() {
       <div className="container">
         <span>&#128075; Hi there</span>
         <h2>I'm a Frontend Developer and Software Engineer from somewhere</h2>
-        <p>I have 3 years of experience of building and developing websites and web applications for various organizations</p> 
+        <p>I have 3 years of experience of building and developing websites and web applications for various organizations</p>
+        <div className="follow">
+          <p>Follow me on:</p>
+          <ul>
+            <li><a href=""></a></li>
+          </ul>
+        </div>
       </div>
     </section>
   )
