cfc7ebf00cb0196757af80042d7b3f97b18a4967
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-16 13:17:19 +0100

    medie breakpoints

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ export default function Banner() {
   return (
     <section className='banner'>
       <div className="container">
-        <span>&#128075; Hi there</span>
+        <span>&#128075; Welcome!</span>
         <h2>I'm a Frontend Developer and Software Engineer from somewhere</h2>
         <p>I have 3 years of experience of building and developing websites and web applications for various organizations</p>
         <div className="follow">
