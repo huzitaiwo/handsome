c753e0cf46641ff44e8ee37d60a5821dce5a501c
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-17 18:36:36 +0100

    creating useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,3 +1,12 @@
-const export useFetch () => {
-    
+import { useState, useEffect } from 'react'
+
+export const useFetch = () => {
+  const [data, setData] = useState([])
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+      
+  }, [])
+
 }
