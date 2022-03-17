0d3a37e9b67654fcbccfec64a4732b3283b51fd0
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-17 12:45:34 +0100

    ThemeContext created

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,8 +3,8 @@ import { createContext } from 'react'
 export const ThemeContext = createContext()
 
 export function ThemeProvider({ children }) {
-  return (
-    <ThemeContext.Provider value={{ color: 'blue'}}>
+  return(
+    <ThemeContext.Provider>
       {children}
     </ThemeContext.Provider>
   )
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ThemeContext } from './context/useThemeContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeContext>
-      <App />
-    </ThemeContext>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
