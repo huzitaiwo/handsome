8d624662449e08a9e25f85c589f8f82c0536406a
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-17 01:03:17 +0100

    created useThemeContext.js file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeContext } from './context/useThemeContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext>
+      <App />
+    </ThemeContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
