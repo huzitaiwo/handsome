866a4686057a31eea64d8e7987ad6d9c3024fdbc
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-17 18:46:12 +0100

    creating useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,7 +6,25 @@ export const useFetch = () => {
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
-      
+    const fetchData = url => {
+      isLoading(true)
+
+      try {
+
+      }
+      catch (err) {
+        setError(err.message)
+      }
+      const res = await fetch(url)
+
+      if(!res.ok) {
+        throw new Error(res.statusText)
+      }
+
+      const json = await res.json()
+
+
+    }
   }, [])
 
 }
