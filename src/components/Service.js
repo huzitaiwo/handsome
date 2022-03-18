464eb81bdf101478b46c2d846371bb209bf71250
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-03-18 19:03:33 +0100

    using useFecth hook

--- src/components/Service.js
+++ src/components/Service.js
@@ -9,18 +9,22 @@ export default function Service() {
 
   return (
     <section className='service'>
-      <h2>What I do</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam atque aliquam laboriosam expedita ea architecto!</p>
+      <div className="container">
+        <h2>What I do</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam atque aliquam laboriosam expedita ea architecto!</p>
 
-      <div className="service-list">
-        {error && <h3>{error}</h3>}
-        {isLoading && <h3>loading...</h3>}
-        {services && services.map(service => (
-          <div key={service.id}>
-            <h2 className='title'>{service.title}</h2>
-            <p className='description'>{service.description}</p>
-          </div>
-        ))}
+        <div className="service-list">
+          {error && <h3>{error}</h3>}
+          {isLoading && <h3>loading...</h3>}
+          {services && services.map(service => (
+            <div key={service.id}>
+              <div  className='title'>
+                <h2>{service.title}</h2>
+              </div>
+              <p className='description'>{service.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
