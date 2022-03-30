d954e653eb416d7282a8231a5b31aeef898d7994
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-30 15:16:13 +0100

    fixed git bug

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,12 +1,28 @@
-import { useFetch } from '../hooks/useFetch'
+import { useState } from 'react'
 import Url from '../asset/2hGjmgZrS1nlsEl5PZorn7EsmzH.jpg'
 // styles
 import './Service.css'
 
 export default function Service() {
-  const url = 'http://localhost:5000/services';
-  const { isLoading, error, data: services } = useFetch(url)
-
+  const [services, setServices] = useState(
+    [
+      {
+        "id": "1",
+        "title": "Web Design and Development",
+        "description": "Nunc eleifend tristique nunc vel ultricies. Sed interdum nisi dignissim mi tristique, sed cursus arcu feugiat. Mauris ex nisi, ullamcorper et eros et, aliquet placerat ex. Nam metus urna, feugiat ac libero vitae."
+      },
+      {
+        "id": "2",
+        "title": "Web3.0 and Blockhain Development",
+        "description": "Etiam vel ante risus. Integer ultrices egestas lacus id faucibus. Suspendisse eleifend nibh eu faucibus consectetur. Etiam ut mi tristique, dignissim sem in, vestibulum dui. Aliquam tempor facilisis tortor a fermentum."
+      },
+      {
+        "id": "3",
+        "title": "Webflow magic",
+        "description": "Aliquam non enim efficitur, feugiat urna quis, placerat ex. Cras risus lorem, placerat vitae hendrerit non, elementum at mi. Nam massa nunc, pharetra bibendum massa et, pellentesque pharetra risus."
+      }
+    ]
+  )
 
   return (
     <section className='service'>
@@ -15,10 +31,8 @@ export default function Service() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam atque aliquam laboriosam expedita ea architecto!</p>
 
         <div className="service-list">
-          {error && <h3>{error}</h3>}
-          {isLoading && <h3>loading...</h3>}
           <ul className='service-list__content'>
-            {services && services.map(service => (
+            {services.map(service => (
               <li key={service.id}>
                 <img src={Url} alt={Url} style={{ filter: 'blur(5px)' }} />
                 <div className="content">
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,4 +1,4 @@
-import Sidebar from '../../components/Sidebar'
+import projectImage from '../../asset/2hGjmgZrS1nlsEl5PZorn7EsmzH.jpg'
 
 //styles
 import './Project.css'
@@ -6,7 +6,27 @@ import './Project.css'
 export default function Project() {
   return (
     <div className='project'>
-      <Sidebar />
+      <div className='project__bar'>
+        <ul className='project__links'>
+          <li><button>Websites</button></li>
+          <li><button>Apps</button></li>
+          <li><button>dApps</button></li>
+        </ul>
+    </div>
+    <div className="project__list">
+      <img 
+        src={projectImage} alt="image"
+        style={{ width: '100%', filter: 'blur(3px)' }}
+      />
+      <img 
+        src={projectImage} alt="image"
+        style={{ width: '100%', filter: 'blur(3px)' }}
+      />
+      <img 
+        src={projectImage} alt="image"
+        style={{ width: '100%', filter: 'blur(3px)' }}
+      />
+    </div>
     </div>
   )
 }
