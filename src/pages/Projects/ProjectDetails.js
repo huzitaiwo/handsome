dd2f2da8daa835596c4b700d5b773508da10f18e
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-04 20:47:43 +0100

    project details page

--- src/pages/Projects/ProjectDetails.js
+++ src/pages/Projects/ProjectDetails.js
@@ -6,35 +6,35 @@ import './ProjectDetails.css'
 export default function ProjectDetails() {
     const { id } = useParams()
 
-  return (
-    <div>
-      <section>
-        <div class="container">
-            <h1 class="project-heading">Developer Website</h1>
-            <p class="project-heading secondary">Web design</p>
-        </div>
-        {/* <img src="./img/edie-main.png" alt="" class="project-main-image" /> */}
-    </section>
-    <section>
-        <div class="container">
-            <div class="project-grid">
-                <div class="project-information">
-                    <p class="project-label">Client</p>
-                    <p class="project-text">Bryan Weird</p>
-                    <p class="project-label">Project type</p>
-                    <p class="project-text">Web design</p>
-                    <p class="project-label">Project year</p>
-                    <p class="project-text">2021</p>
+    return (
+        <div>
+            <section>
+                <div class="container">
+                    <h1 class="project-heading">Developer Website</h1>
+                    <p class="project-heading secondary">Web design</p>
                 </div>
-                <div class="project-description">
-                    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita omnis, excepturi nisi architecto fugit autem iusto. Aspernatur dolorum expedita soluta, sit error ratione modi amet itaque dicta veritatis iusto tempora hic debitis commodi exercitationem doloremque aut ipsam explicabo ipsa nobis?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita omnis, excepturi nisi architecto fugit autem iusto. Aspernatur dolorum expedita soluta, sit error ratione modi amet itaque dicta veritatis iusto tempora hic debitis commodi exercitationem doloremque aut ipsam explicabo ipsa nobis?</p>
-                    <a class="project-link btn" href="https://edieweb.netlify.app" target="_blank">View project <img src="./icon/arrow-right.svg" alt="avist website icon" /></a>
+                {/* <img src="./img/edie-main.png" alt="" class="project-main-image" /> */}
+            </section>
+            <section>
+                <div class="container">
+                    <div class="project-grid">
+                        <div class="project-information">
+                            <p class="project-label">Client</p>
+                            <p class="project-text">Bryan Weird</p>
+                            <p class="project-label">Project type</p>
+                            <p class="project-text">Web design</p>
+                            <p class="project-label">Project year</p>
+                            <p class="project-text">2021</p>
+                        </div>
+                        <div class="project-description">
+                            <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita omnis, excepturi nisi architecto fugit autem iusto. Aspernatur dolorum expedita soluta, sit error ratione modi amet itaque dicta veritatis iusto tempora hic debitis commodi exercitationem doloremque aut ipsam explicabo ipsa nobis?</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita omnis, excepturi nisi architecto fugit autem iusto. Aspernatur dolorum expedita soluta, sit error ratione modi amet itaque dicta veritatis iusto tempora hic debitis commodi exercitationem doloremque aut ipsam explicabo ipsa nobis?</p>
+                            <a class="project-link btn" href="https://edieweb.netlify.app" target="_blank">View project <img src="./icon/arrow-right.svg" alt="avist website icon" /></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
-    </section>
-    </div>
-  )
+    )
 }
