078e6badf382eee329d2b313934461618a8f685e
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-04 20:48:54 +0100

    added vecteezy imags

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,6 @@
 import './Banner.css'
 
+// images
 import linkedin from '../asset/icon/linkedin-brand.svg'
 import github from '../asset/icon/github-brand.svg'
 import instagram from '../asset/icon/instagram-brand.svg'
@@ -9,17 +10,19 @@ export default function Banner() {
   return (
     <section className='banner'>
       <div className="container">
-        <span>&#128075; Welcome!</span>
-        <h2>I'm a Frontend Developer and Software Engineer from somewhere</h2>
-        <p>I have <small>2 years</small> of experience of creating functional high-performing, content-driven, optimized websites and web applications.</p>
-        <div className="follow">
-          <p>Follow me on:</p>
-          <ul className='social-links'>
-            <li><a href="https://google.com"><img src={github} alt="instagram" /></a></li>
-            <li><a href="https://google.com"><img src={linkedin} alt="linkedin" /></a></li>
-            <li><a href="https://google.com"><img src={twitter} alt="twitter" /></a></li>
-            <li><a href="https://google.com"><img src={instagram} alt="instagram" /></a></li>
-          </ul>
+        <div className="container__content">
+          <span>&#128075; Welcome!</span>
+          <h2>I'm a Frontend Developer and Software Engineer from somewhere</h2>
+          <p>I have <small>2 years</small> of experience of creating functional high-performing, content-driven, optimized websites and web applications.</p>
+          <div className="follow">
+            <p>Follow me on:</p>
+            <ul className='social-links'>
+              <li><a href="https://google.com"><img src={github} alt="instagram" /></a></li>
+              <li><a href="https://google.com"><img src={linkedin} alt="linkedin" /></a></li>
+              <li><a href="https://google.com"><img src={twitter} alt="twitter" /></a></li>
+              <li><a href="https://google.com"><img src={instagram} alt="instagram" /></a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </section>
