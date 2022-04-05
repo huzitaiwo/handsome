cda93e435bc020823af0c8a02cbd3b9a68928cd1
Im-Hassan-wd <alhassansahih@gmail.com>
2022-04-05 11:20:55 +0100

    project list

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+
+// images
 import projectManagement from '../../asset/project.png'
 import myMovie from '../../asset/mymovie.png'
 
@@ -5,21 +8,35 @@ import myMovie from '../../asset/mymovie.png'
 import './Project.css'
 
 export default function Project() {
+  const apps = [
+    { id: 1, image: projectManagement },
+    { id: 2, image: projectManagement }
+  ]
+  const websites = [
+    { id: 1, image: myMovie },
+    { id: 2, image: myMovie }
+  ]
+
+  const [projects, setProjects] = useState(websites)
+
   return (
     <div className='project'>
       <div className="container">
         <div className='project__bar'>
           <div className="projecr__bar-content">
             <ul className='project__links'>
-              <li><button className='active'>Websites</button></li>
-              <li><button>Apps</button></li>
-              <li><button>dApps</button></li>
+              <li><button className='active' onClick={() => setProjects(websites)}>Websites</button></li>
+              <li><button onClick={() => setProjects(apps)}>Apps</button></li>
+              <li><button onClick={() => setProjects([])}>dApps</button></li>
             </ul>
           </div>
         </div>
         <div className="project__list">
-          <img src={myMovie} alt="my movie website" />
-          <img src={projectManagement} alt="project management web app" />
+          {projects && projects.map(project => (
+            <img key={project.id} src={project.image} alt="my movie website" />
+          ))}
+          {/* <img src={myMovie} alt="my movie website" />
+          <img src={projectManagement} alt="project management web app" /> */}
         </div>
       </div>
     </div>
