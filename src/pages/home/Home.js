df291bd988e0e08c46f011c21dbf084ef032b9ef
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-09 14:52:19 +0100

    reponsive contact page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,16 @@
 import './Home.css'
 
-// components
+// components & pages
 import Banner from '../../components/Banner'
 import Service from '../../components/Service'
+import Contact from '../contact/Contact'
 
 export default function Home() {
   return (
     <div>
       <Banner />
       <Service />
+      <Contact />
     </div>
   )
 }
