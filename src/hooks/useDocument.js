f983e38a8292ea6a0eacdcd952dde8452efaf258
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-10 15:28:16 +0100

    set useDocument loading state

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -3,14 +3,17 @@ import { Firestore } from '../firebase/config'
 
 export const useDocument = (collection, id) => {
   const [document, setDocument] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
   // realtime data for document
   useEffect(() => {
+    setIsLoading(true)
     const ref = Firestore.collection(collection).doc(id)
 
     const unsubscribe = ref.onSnapshot(snapshot => {
       if (snapshot.data()) {
+        setIsLoading(false)
         setDocument({ ...snapshot.data(), id: snapshot.id })
         setError(null)
       }
@@ -18,14 +21,15 @@ export const useDocument = (collection, id) => {
         setError('no such document')
       }
     }, (err) => {
-      console.log(err.message)
+      setIsLoading(false)
       setError('failed to get document')
+      console.log(err.message)
     })
 
     return () => unsubscribe()
 
   }, [collection, id])
 
-  return { document, error }
+  return { document, error, isLoading }
 
 } 
--- src/pages/Projects/ProjectDetails.js
+++ src/pages/Projects/ProjectDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import arrowRight from '../../asset/icon/arrow-right.svg'
+import { useDocument } from '../../firebase/config'
 
 // styles
 import './ProjectDetails.css'
