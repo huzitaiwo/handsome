f1dfe345324fd55b798b883cf210f5ee0f3afe4c
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-10 15:36:37 +0100

    setup dynamic project details page

--- src/pages/Projects/ProjectDetails.js
+++ src/pages/Projects/ProjectDetails.js
@@ -16,34 +16,40 @@ export default function ProjectDetails() {
     return <h3>{error}</h3>
   }
 
+  console.log(project)
+
   return (
     <div className='project-details'>
-      <section>
-        <div className="container">
-          <h1 className="project-heading">{project.name}</h1>
-          <p className="project-heading secondary">{project.type}</p>
-        </div>
-        <img src={project.photoURL} alt={project.name} className="project-main-image" />
-      </section>
-      <section>
-        <div className="container">
-          <div className="project-grid">
-            <div className="project-information">
-              <p className="project-label">Client</p>
-              <p className="project-text">{project.clientName}</p>
-              <p className="project-label">Project type</p>
-              <p className="project-text">{project.type}</p>
-              <p className="project-label">Project year</p>
-              <p className="project-text">{project.year}</p>
+      {project && (
+        <>
+          <section>
+            <div className="container">
+              <h1 className="project-heading">{project.name}</h1>
+              <p className="project-heading secondary">{project.type}</p>
             </div>
-            <div className="project-description">
-              <h2>{project.descriptionTitle}</h2>
-              <p>{project.description}</p>
-              <a className="project-link btn" href={project.URL} target="_blank" rel="noreferrer">View project <img src={arrowRight} alt="visit website" /></a>
+            <img src={project.photoURL} alt={project.name} className="project-main-image" />
+          </section>
+          <section>
+            <div className="container">
+              <div className="project-grid">
+                <div className="project-information">
+                  <p className="project-label">Client</p>
+                  <p className="project-text">{project.clientName}</p>
+                  <p className="project-label">Project type</p>
+                  <p className="project-text">{project.type}</p>
+                  <p className="project-label">Project year</p>
+                  <p className="project-text">{project.year}</p>
+                </div>
+                <div className="project-description">
+                  <h2>{project.descriptionTitle}</h2>
+                  <p>{project.description}</p>
+                  <a className="project-link btn" href={project.URL} target="_blank" rel="noreferrer">View project <img src={arrowRight} alt="visit website" /></a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        </>
+      )}
     </div>
   )
 }
