d8aa267e92aa79b0247d73a66108afb60fa4f618
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-10 14:55:27 +0100

    firebaseconfig

---
+++ firebase/Config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBS2HK5QC4U5Jl51uOQx_foB1y4osjvy2M",
+  authDomain: "hussen-portfolio.firebaseapp.com",
+  projectId: "hussen-portfolio",
+  storageBucket: "hussen-portfolio.appspot.com",
+  messagingSenderId: "934384552556",
+  appId: "1:934384552556:web:5f7b8e797fb445ebcd2649"
+};
