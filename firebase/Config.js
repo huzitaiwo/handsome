880b6ea63e9efde7a8f4455eff63598920e08e97
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-10 15:02:57 +0100

    firebaseconfig

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/storage'
+
 const firebaseConfig = {
   apiKey: "AIzaSyBS2HK5QC4U5Jl51uOQx_foB1y4osjvy2M",
   authDomain: "hussen-portfolio.firebaseapp.com",
@@ -6,3 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "934384552556",
   appId: "1:934384552556:web:5f7b8e797fb445ebcd2649"
 };
+
+//initialize firebase
+firebase.initializeApp(firebaseConfig)
