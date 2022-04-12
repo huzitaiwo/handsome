315fde957a33b8b537eaf6aebe54d94e1c3dfda0
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-12 11:48:14 +0100

    added public images

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useFetch } from '../../hooks/useFetch'
-// import image from '../../../public/'
+import image from '../../../public/fundo.png'
 
 // images
 import projectManagement from '../../asset/images/project.png'
