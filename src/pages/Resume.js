fc7284f79cd0e0993f567333a633860ca3fa34cc
Im-Hassan-wd <taiwohusayn5@gmail.com>
2022-04-14 12:19:36 +0100

    resume page

---
+++ src/pages/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import resume from '../asset/cv.hussen_taiwo.pdf'
+
+export default function Resume() {
+  return (
+    <div>
+      <img src={resume} alt="resume" />
+    </div>
+  )
+}
